6682d9d9d66bf897a61477c2af08b87e373880a0
Mahendran-Murugan <mahendran1482004@gmail.com>
2024-05-17 19:29:33 +0530

    feat: add table layout and use list rendering

--- client/src/App.js
+++ client/src/App.js
@@ -3,29 +3,70 @@ import io from 'socket.io-client'
 import { Input } from './components/Input';
 
 function App() {
-  const socket = io('localhost:3000');
+  const socket = io('localhost:5000');
   const [details, setDetails] = useState({});
+  const [serverData, setServerData] = useState([]);
+
+  const connectSocket = (socket) => {
+    console.log(socket);
+  }
+
+  const handlePlayerDetails = (playerDetails) => {
+    setServerData(playerDetails);
+  }
 
   useEffect(() => {
-    socket.on('connection', (socket) => {
-      console.log(socket);
-    })
+    socket.on('connection', connectSocket);
+    socket.on('playerDetails', handlePlayerDetails);
+    return () => {
+      socket.off('connection', connectSocket);
+      socket.off('playerDetails', handlePlayerDetails);
+    }
   }, [socket])
 
   const handleInput = (e) => {
     let { name, value } = e.target;
     let values = { [name]: value };
     setDetails((prev) => ({ ...prev, ...values }));
-    console.log(values);
+  }
+
+  const handleSetScore = (e) => {
+    e.preventDefault();
+    console.log(details);
+    socket.emit('details', details);
   }
 
   return (
-    <div className='flex h-screen items-center justify-center bg-primary'>
-      <form className='w-96 flex p-8 rounded-md flex-col items-center justify-center bg-background gap-6'>
-        <h1 className='text-3xl font-semibold my-6 text-primary'>Multiplayer Dashboard</h1>
+    <div className='flex flex-col h-screen items-center justify-center gap-6 bg-primary'>
+      <h1 className='text-5xl font-bold my-6 bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 inline-block text-transparent bg-clip-text'>Multiplayer Dashboard</h1>
+      <form className='w-96 flex p-8 rounded-md flex-col items-center justify-center bg-background gap-6' onSubmit={handleSetScore}>
         <Input name="name" placeholder={"Enter your name"} handleInput={handleInput} />
         <Input name="score" placeholder={"Enter your Score"} handleInput={handleInput} />
+        <button className="bg-primary text-background px-4 py-2 rounded-lg" type='submit'>Publish Score</button>
       </form>
+      <h2 className='text-2xl font-semibold my-6 text-background'>Player Details Table</h2>
+      <div className='rounded-md shadow overflow-hidden'>
+        {
+          serverData && <table className='w-96 bg-background'>
+            <thead className='bg-background border-b-2 border-gray-50'>
+              <tr>
+                <th className='w-8 text-sm font-semibold tracking-wider p-3 text-left text-black'>No.</th>
+                <th className='w-20 text-sm font-semibold tracking-wider p-3 text-left text-black'>Name</th>
+                <th className='w-24 text-sm font-semibold tracking-wider p-3 text-left text-black'>Score</th>
+              </tr>
+            </thead>
+            <tbody>
+              {serverData.map((data, index) => {
+                return (<tr key={index} className={index % 2 === 0 ? "bg-white" : "bg-gray"}>
+                  <td className='text-sm p-3 text-gray-700 whitespace-nowrap'>{index + 1}</td>
+                  <td className='text-sm p-3 text-gray-700 whitespace-nowrap'>{data.name}</td>
+                  <td className='text-sm p-3 text-gray-700 whitespace-nowrap'>{data.score}</td>
+                </tr>)
+              })}
+            </tbody>
+          </table>
+        }
+      </div>
     </div>
   );
 }
--- client/src/components/Input.js
+++ client/src/components/Input.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const Input = ({ name, handleInput, placeholder }) => {
     return (
         <div>
-            <input className='border-2 border-secondary py-2 px-6 rounded-lg' placeholder={placeholder} name={name} onChange={handleInput} />
+            <input className='border-2 border-secondary py-2 px-6 w-full rounded-lg' placeholder={placeholder} name={name} onChange={handleInput} />
         </div>
     )
 }
