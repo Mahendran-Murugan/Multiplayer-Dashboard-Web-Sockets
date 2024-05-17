c030bec91cdfdb07d3940563dbdccb4ebc2b6730
Mahendran-Murugan <mahendran1482004@gmail.com>
2024-05-17 12:52:41 +0530

    feat: add inital files for websockets

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <>
+      <h1>React Multiplayer Dashboard</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ server/src/index.mjs
@@ -0,0 +1,26 @@
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const PORT = process.env.PORT || 7000;
+
+const httpServer = createServer();
+
+const socketServer = new Server(httpServer, {
+    cors: {
+        origin: [
+            "http://127.0.0.1:5000",
+            "http://127.0.0.1:7000"
+        ]
+    }
+});
+
+socketServer.on('connection', (socket) => {
+    console.log(socket);
+})
+
+httpServer.listen(PORT, () => {
+    console.log(`Server is Running on http://localhost:${PORT}`)
+})
